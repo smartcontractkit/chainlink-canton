78752e4e7c74abde621a95576c4a465bc5215930
Friedemann Fürst <59653747+friedemannf@users.noreply.github.com>
2026-02-12 01:37:24 +0100

    Add Execute choice + Sender ops

--- deployment/operations/ccip/receiver/ccip_receiver.go
+++ deployment/operations/ccip/receiver/ccip_receiver.go
@@ -29,3 +29,15 @@ var Deploy = contract.NewDeploy(contract.DeployParams[ccipreceiver.CCIPReceiver]
 	PackageName: string(contracts.CCIPReceiver),
 	Prefix:      "ccipreceiver",
 })
+
+var Execute = contract.NewExercise(contract.ExerciseParams[ccipreceiver.Execute2]{
+	Name:         "canton/ccip/receiver/execute",
+	Version:      Version,
+	Description:  "Calls the Execute choice on a CCIP Receiver contract",
+	ContractType: ContractType,
+	Validate: func(input ccipreceiver.Execute2) error {
+		return nil
+	},
+	Template: ccipreceiver.CCIPReceiver{},
+	Method:   ccipreceiver.CCIPReceiver{}.Execute,
+})
