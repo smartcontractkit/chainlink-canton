40860c1de78a721cd17ef5c5d22499fcb0a0cd3e
cfal <cfal@users.noreply.github.com>
2026-03-30 23:17:57 +0800

    ccip/devenv/impl.go: update

--- ccip/devenv/impl.go
+++ ccip/devenv/impl.go
@@ -1241,10 +1241,18 @@ func (c *Chain) SendMessage(ctx context.Context, dest uint64, fields cciptestint
 				V3: &ccipclient.GenericExtraArgsV3{
 					GasLimit:           types.INT64(opts.ExecutionGasLimit),
 					BlockConfirmations: 0,
-					Ccvs:               senderRequiredCCVs,
+					Ccvs: senderRequiredCCVs,
+					ExecutorType: ccipclient.ExecutorType{
+						ExecutorWithAddress: &ccipclient.ExecutorWithAddress{
+							ExecutorAddress: mcmsbindings.RawInstanceAddress{
+								Unpack: types.TEXT(executorInstanceID.String() + "@" + party),
+							},
+						},
+					},
 					Executor: &ccipclient.ExecutorInput{
-						ExecutorCid:  executorCID,
-						ExecutorArgs: types.TEXT(""),
+						ExecutorCid:         executorCID,
+						ExecutorArgs:        types.TEXT(""),
+						ExecutorExtraContext: common.CCIPContext{Values: types.TEXTMAP{}},
 					},
 					TokenReceiver: types.TEXT(""),
 					TokenArgs:     types.TEXT(""),
