c996dff0a79944e4c4b53c0151ce8038a2c6c91f
cfal <cfal@users.noreply.github.com>
2026-03-30 22:38:03 +0800

    integration-tests/ccip: update

--- integration-tests/ccip/ccip_send_with_token_test.go
+++ integration-tests/ccip/ccip_send_with_token_test.go
@@ -884,9 +884,17 @@ func TestCCIPSendWithTokenTransferFeeBps(t *testing.T) {
 					Ccvs: []mcms.RawInstanceAddress{
 						{Unpack: types.TEXT(committeeVerifierRawAddr.String())},
 					},
+					ExecutorType: ccipclient.ExecutorType{
+						ExecutorWithAddress: &ccipclient.ExecutorWithAddress{
+							ExecutorAddress: mcms.RawInstanceAddress{
+								Unpack: types.TEXT("test-executor@" + partyCCIP),
+							},
+						},
+					},
 					Executor: &ccipclient.ExecutorInput{
-						ExecutorCid:  types.CONTRACT_ID(executorCid),
-						ExecutorArgs: types.TEXT(""),
+						ExecutorCid:          types.CONTRACT_ID(executorCid),
+						ExecutorArgs:         types.TEXT(""),
+						ExecutorExtraContext: common.CCIPContext{Values: types.TEXTMAP{}},
 					},
 					TokenReceiver: types.TEXT(""),
 					TokenArgs:     types.TEXT(""),
