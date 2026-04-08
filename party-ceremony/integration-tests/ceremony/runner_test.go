e4805676de99f88ed16081291eacfa7436800e01
RodrigoAD <15104916+RodrigoAD@users.noreply.github.com>
2026-04-08 14:11:04 +0100

    reactivate int tests

--- party-ceremony/integration-tests/ceremony/runner_test.go
+++ party-ceremony/integration-tests/ceremony/runner_test.go
@@ -8,13 +8,13 @@ import (
 )
 
 func TestCeremonies(t *testing.T) {
-	// t.Run("CantonOnboardingFlowTestSuite", func(t *testing.T) {
-	// 	suite.Run(t, new(OnboardingFlowTestSuite))
-	// })
+	t.Run("CantonOnboardingFlowTestSuite", func(t *testing.T) {
+		suite.Run(t, new(OnboardingFlowTestSuite))
+	})
 
-	// t.Run("CantonKickFlowTestSuite", func(t *testing.T) {
-	// 	suite.Run(t, new(KickFlowTestSuite))
-	// })
+	t.Run("CantonKickFlowTestSuite", func(t *testing.T) {
+		suite.Run(t, new(KickFlowTestSuite))
+	})
 
 	t.Run("CantonContractDeployFlowTestSuite", func(t *testing.T) {
 		suite.Run(t, new(ContractDeployFlowTestSuite))
