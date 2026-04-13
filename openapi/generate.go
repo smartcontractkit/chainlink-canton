364b272f0e29aff64d1255d463bca3129eafea35
Friedemann Fürst <59653747+friedemannf@users.noreply.github.com>
2026-04-13 15:12:45 +0200

    Revert removal of old API

--- openapi/generate.go
+++ openapi/generate.go
@@ -3,3 +3,5 @@ package openapi
 //go:generate go tool oapi-codegen -generate models,client -config ./cfg.yaml -o ./gen/tokenMetadataV1/tokenMetadataV1.gen.go -package tokenMetadataV1 ./src/token-metadata-v1.yaml
 //go:generate go tool oapi-codegen -generate models,client -config ./cfg.yaml -o ./gen/transferInstructionV1/transferInstructionV1.gen.go -package transferInstructionV1 ./src/transfer-instruction-v1.yaml
 //go:generate go tool oapi-codegen -generate models,client -config ./cfg.yaml -o ./gen/scanProxy/scanProxy.gen.go -package scanProxy ./src/scan-proxy.yaml
+
+//go:generate go tool oapi-codegen -generate models,client,gin-server -config ./cfg.yaml -o ./gen/eds/eds.gen.go -package edsv1 ./src/eds.yaml
