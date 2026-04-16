d05eb48194dec0a06c5c339e4c04cd519e821498
cfal <cfal@users.noreply.github.com>
2026-04-16 16:42:29 +0000

    integration-tests/coin/coin_test.go: update

--- integration-tests/coin/coin_test.go
+++ integration-tests/coin/coin_test.go
@@ -522,7 +522,7 @@ func TestCoin(t *testing.T) {
 								EntityName: "MintRole",
 							},
 							ContractId: daveMintRoleCid,
-							Choice:     "Mint",
+							Choice:     "MintRole_Mint",
 							ChoiceArgument: &apiv2.Value{Sum: &apiv2.Value_Record{Record: &apiv2.Record{Fields: []*apiv2.RecordField{
 								{
 									Label: "instrumentId",
