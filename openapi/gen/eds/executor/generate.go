73e9542513ac7161137b573e16a80189525131db
Friedemann Fürst <59653747+friedemannf@users.noreply.github.com>
2026-04-16 10:28:19 +0200

    Update EDS API Spec (#374)
    * Update EDS API Spec
    
    * Regenerate
    
    * Revert removal of old API
    
    * Update docs
    
    * Regenerate
    
    * Update API spec
    
    * Update API spec
    
    * Update casing

---
+++ openapi/gen/eds/executor/generate.go
@@ -0,0 +1,3 @@
+package executor
+
+//go:generate go tool oapi-codegen -config ./cfg.yaml ../../../src/eds/executor/eds-executor.yaml
---
+++ openapi/gen/eds/global/generate.go
@@ -0,0 +1,3 @@
+package global
+
+//go:generate go tool oapi-codegen -config ./cfg.yaml ../../../src/eds/global/eds-global.yaml
