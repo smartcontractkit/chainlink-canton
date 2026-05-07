1de2a995268e8f21d8e21ba63de7d117738db28a
Makram Kamaleddine <makramkd@users.noreply.github.com>
2026-05-07 22:05:13 +0300

    common init

--- ccip/devenv/cmd/ccv/main.go
+++ ccip/devenv/cmd/ccv/main.go
@@ -1,29 +1,9 @@
 package main
 
 import (
-	chain_selectors "github.com/smartcontractkit/chain-selectors"
-	ccv "github.com/smartcontractkit/chainlink-ccv/build/devenv"
+	_ "github.com/smartcontractkit/chainlink-canton/ccip/devenv" // register all canton-related devenv components
 	"github.com/smartcontractkit/chainlink-ccv/build/devenv/cli"
-	"github.com/smartcontractkit/chainlink-ccv/build/devenv/registry"
-	"github.com/smartcontractkit/chainlink-ccv/build/devenv/services/chainconfig"
-	"github.com/smartcontractkit/chainlink-ccv/build/devenv/services/committeeverifier"
-
-	cantondevenv "github.com/smartcontractkit/chainlink-canton/ccip/devenv"
 )
-
-func init() {
-	ccv.RegisterLauncher(chain_selectors.FamilyCanton, cantondevenv.NewLauncher())
-	// Register the canton modifier for the canton family.
-	committeeverifier.RegisterModifier(chain_selectors.FamilyCanton, cantondevenv.CommitteeVerifierModifier)
-	// Register the canton chain config loader for the canton family.
-	chainconfig.RegisterChainConfigLoader(chain_selectors.FamilyCanton, cantondevenv.CommitteeVerifierConfigLoader)
-	// Register the canton impl factory for the canton family.
-	ccv.RegisterImplFactory(chain_selectors.FamilyCanton, cantondevenv.NewImplFactory())
-	// Register the canton CLDF provider factory for the canton family.
-	registry.GetGlobalCLDFProviderRegistry().Register(chain_selectors.FamilyCanton, cantondevenv.NewCLDF)
-
-	// The other Canton adapters are registered via the init function in the adapters package
-}
 
 func main() {
 	cli.RunCLI()
