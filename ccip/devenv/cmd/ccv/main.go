13b0a96941cb1fd9f98cbe5f630cd07ae9cb86c7
Makram Kamaleddine <makramkd@users.noreply.github.com>
2026-05-08 09:55:55 +0300

    goimports

--- ccip/devenv/cmd/ccv/main.go
+++ ccip/devenv/cmd/ccv/main.go
@@ -1,8 +1,9 @@
 package main
 
 import (
-	_ "github.com/smartcontractkit/chainlink-canton/ccip/devenv" // register all canton-related devenv components
 	"github.com/smartcontractkit/chainlink-ccv/build/devenv/cli"
+
+	_ "github.com/smartcontractkit/chainlink-canton/ccip/devenv" // register all canton-related devenv components
 )
 
 func main() {
