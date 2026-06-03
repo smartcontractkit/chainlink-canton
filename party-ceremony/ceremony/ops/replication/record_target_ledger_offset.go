23f30c0cb75cc8ad0b8534d6673f5563128413a8
RodrigoAD <15104916+RodrigoAD@users.noreply.github.com>
2026-06-03 12:10:22 +0200

    fix lint

--- party-ceremony/ceremony/ops/replication/record_target_ledger_offset.go
+++ party-ceremony/ceremony/ops/replication/record_target_ledger_offset.go
@@ -8,6 +8,7 @@ import (
 	"github.com/Masterminds/semver/v3"
 
 	"github.com/smartcontractkit/chainlink-canton/party-ceremony/ceremony"
+
 	"github.com/smartcontractkit/chainlink-deployments-framework/operations"
 )
 
