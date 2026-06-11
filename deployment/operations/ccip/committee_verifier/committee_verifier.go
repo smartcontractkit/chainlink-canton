08fa3501bde2fab593657fc6656ff64897ee83f7
stackman27 <sishir.giri@smartcontract.com>
2026-06-10 23:48:25 -0700

    use owner during ccv call

--- deployment/operations/ccip/committee_verifier/committee_verifier.go
+++ deployment/operations/ccip/committee_verifier/committee_verifier.go
@@ -8,7 +8,6 @@ import (
 
 	"github.com/Masterminds/semver/v3"
 
-	"github.com/smartcontractkit/chainlink-deployments-framework/chain/canton"
 	"github.com/smartcontractkit/chainlink-deployments-framework/deployment"
 	"github.com/smartcontractkit/go-daml/pkg/types"
 
@@ -155,12 +154,6 @@ var ApplyAllowListUpdates = contract.NewExercise(contract.ExerciseParams[committ
 	Description:  "Applies allow lists updates to a Canton CommitteeVerifier instance",
 	ContractType: ContractType,
 	Validate:     nil,
-	Modifier: func(chain canton.Chain, input committeeverifier.ApplyAllowListUpdates) (committeeverifier.ApplyAllowListUpdates, error) {
-		// Automatically set the caller
-		input.Caller = types.PARTY(chain.Participants[0].PartyID)
-
-		return input, nil
-	},
 	Template:     committeeverifier.CommitteeVerifier{},
 	Method:       committeeverifier.CommitteeVerifier{}.ApplyAllowListUpdates,
 	EncodeMethod: ccvsEncoder.ApplyAllowListUpdates,
