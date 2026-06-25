f914c1483c3f23f208d5fbcc8ba956b46f440831
Sishir Giri <sishirg27@gmail.com>
2026-06-24 20:02:23 -0700

    Canton: Cleanup executor  (#718)
    * cleanup executor
    
    * bump go mod
    
    * bump go mod
    
    * bump go mod
    
    * fix go mod

--- deployment/changesets/offchain_allowing_canton_min_nops.go
+++ deployment/changesets/offchain_allowing_canton_min_nops.go
@@ -7,25 +7,6 @@ import (
 	"github.com/smartcontractkit/chainlink-deployments-framework/deployment"
 )
 
-// ApplyExecutorConfigAllowingCantonMinNOPs wraps apply-executor-config for prod Canton lanes:
-//   - skips production min-NOP topology validation during VerifyPreconditions (Canton committee/pool)
-//   - skips JD ListNodeChainConfigs validation during Apply so Canton destination blocks can be
-//     pushed to EVM ccvexecutor jobs before Canton node chain configs exist in JD
-func ApplyExecutorConfigAllowingCantonMinNOPs(registry *adapters.ExecutorConfigRegistry) deployment.ChangeSetV2[v2_0_0.ApplyExecutorConfigInput] {
-	inner := v2_0_0.ApplyExecutorConfig(registry, adapters.GetChainFamilyRegistry())
-
-	return deployment.CreateChangeSet(
-		func(e deployment.Environment, cfg v2_0_0.ApplyExecutorConfigInput) (deployment.ChangesetOutput, error) {
-			return applyExecutorConfigSkippingChainSupportValidation(registry, e, cfg)
-		},
-		func(e deployment.Environment, cfg v2_0_0.ApplyExecutorConfigInput) error {
-			return WithCantonProductionMinNOPCheckBypassed(cfg.Topology, func() error {
-				return inner.VerifyPreconditions(e, cfg)
-			})
-		},
-	)
-}
-
 // ApplyVerifierConfigAllowingCantonMinNOPs wraps apply-verifier-config with the same Canton production
 // minimum-NOP bypass as lane configure and apply-executor-config.
 func ApplyVerifierConfigAllowingCantonMinNOPs(registry *adapters.VerifierConfigRegistry) deployment.ChangeSetV2[v2_0_0.ApplyVerifierConfigInput] {
