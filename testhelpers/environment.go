78ae20655f8822cd72d6272533299119da69432c
Friedemann Fürst <59653747+friedemannf@users.noreply.github.com>
2026-08-17 20:00:54 +0200

    Bump Splice to 0.7.3 (#880)

--- testhelpers/environment.go
+++ testhelpers/environment.go
@@ -23,7 +23,7 @@ import (
 const ParticipantInputEnvVar = "PARTICIPANT_INPUT"
 
 // SpliceVersion overrides the default version specified by CTF
-const SpliceVersion = "0.7.2"
+const SpliceVersion = "0.7.3"
 
 type TestEnvironment struct {
 	Logger zerolog.Logger
